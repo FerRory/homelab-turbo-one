d8fc64e918af0101a02cef494e39a6597ee8d535
FerRory <rory@ferrory.net>
2025-09-01 17:58:04 +0200

    Small improvements

--- build-iso.sh
+++ build-iso.sh
@@ -7,12 +7,12 @@ podman run \
 --tty \
 --privileged \
 --security-opt label=type:unconfined_t \
--v /Users/roryschellekens/workspace/ferrory-homelab/servers/turbo-one/output:/output/ \
+-v ./output:/output/ \
 -v /var/lib/containers/storage:/var/lib/containers/storage \
--v /Users/roryschellekens/workspace/ferrory-homelab/servers/turbo-one/config.toml:/config.toml:ro \
+-v ./config.toml:/config.toml:ro \
 --label bootc.image.builder=true \
 quay.io/centos-bootc/bootc-image-builder:latest \
-localhost/homelab-turbo-1:latest \
+ghcr.io/ferrory/homelab-turbo-one:latest \
 --output /output/ \
 --type anaconda-iso \
 --target-arch arm64 \
